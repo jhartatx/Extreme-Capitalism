df58e59686cf9708a12595ecf62597d203cccb83
dwc3k5 <dwc1608@gmail.com>
2017-11-16 23:45:47 -0600

    basic folder structure and server/database functionality

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS todolist;
+DROP DATABASE IF EXISTS c6mw1yoy0se23py8;
 -- Creates the "todolist" database --
-CREATE DATABASE todolist;
+CREATE DATABASE c6mw1yoy0se23py8;
---
+++ server.js
@@ -0,0 +1,47 @@
+var mysql = require("mysql");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var exphbs = require("express-handlebars");
+
+
+
+var connection ;
+
+//CONNECTION TO DATABASE
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host:"localhost",
+    port:3307,
+    user:"root",
+    password:"",
+    database:"c6mw1yoy0se23py8"
+  });
+}
+connection.connect();
+module.exports = connection;
+
+var app = express();
+//HANDLEBARS
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+//CONNECTION TO SERVER
+
+var PORT = process.env.PORT || 8081;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
