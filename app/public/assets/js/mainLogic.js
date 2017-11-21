780017dcf38823668d1186ba9b9a44ee62b1886a
jhartatx <jhartatx@gmail.com>
2017-11-21 10:24:35 -0600

    dice complete

--- app/public/assets/js/mainLogic.js
+++ app/public/assets/js/mainLogic.js
@@ -12,9 +12,6 @@ $.get("/pullcommunity").then(function(response){
   console.log(response);
 });
 
-
-
-
 /*AJAX UPDATE pases the move variable through and sends information to the api routes. triggering the database the upset targeting the current player with the "active" boolean set to true*/
 //this variable should be set to the players new location via a function
 
@@ -22,11 +19,8 @@ $.get("/pullcommunity").then(function(response){
   function updateMove(move) {
     console.log(move);
     $.ajax({
-      //type of request sent
       method: "PUT",
-      //target of request in apiroutes
       url: "/playermove",
-      //the move variable gets passed through here
       data: {move:move}
     }).done(console.log("finished"));
 }
@@ -36,34 +30,68 @@ $.get("/pullcommunity").then(function(response){
 var dbl = 0;
 function rolldice() {
   $.get("/checkactiveplayer").then(function(response){
-    currentLoc = response[0].pos_id;
-    console.log("current "+ currentLoc);
+    currentLocation = response[0].pos_id;
+    console.log("current location: "+ currentLocation);
 
-    var x = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
-    var y = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
+    var x = Math.floor(Math.random() * 6 + 1);
+    var y = Math.floor(Math.random() * 6 + 1);
 
     console.log("dice1: " + x);
     console.log("dice2: " + y);
 
     var diceTotal = x + y;
-    var newPos = currentLoc + diceTotal;
+    var newPosition = currentLocation + diceTotal;
 
-    console.log("newPos"+newPos);
-    updateMove(newPos);
-    console.log("dice total: " + diceTotal);
-    updateMove(diceTotal);
+    updateMove(newPosition);
+
+//assign dice photo to dice role and display
+    if(x == 1){
+      $("#dice-1 img").attr('src', "./assets/images/dice-sides/side1.jpg");
+    }
+    if(x == 2){
+      $("#dice-1 img").attr('src', "./assets/images/dice-sides/side2.jpg");
+    }
+    if(x == 3){
+      $("#dice-1 img").attr('src', "./assets/images/dice-sides/side3.jpg");
+    }
+    if(x == 4){
+      $("#dice-1 img").attr('src', "./assets/images/dice-sides/side4.jpg");
+    }
+    if(x == 5){
+      $("#dice-1 img").attr('src', "./assets/images/dice-sides/side5.jpg");
+    }
+    if(x == 6){
+      $("#dice-1 img").attr('src', "./assets/images/dice-sides/side6.jpg");
+    }
+    if(y == 1){
+      $("#dice-2 img").attr('src', "./assets/images/dice-sides/side1.jpg");
+    }
+    if(y == 2){
+      $("#dice-2 img").attr('src', "./assets/images/dice-sides/side2.jpg");
+    }
+    if(y == 3){
+      $("#dice-2 img").attr('src', "./assets/images/dice-sides/side3.jpg");
+    }
+    if(y == 4){
+      $("#dice-2 img").attr('src', "./assets/images/dice-sides/side4.jpg");
+    }
+    if(y == 5){
+      $("#dice-2 img").attr('src', "./assets/images/dice-sides/side5.jpg");
+    }
+    if(y == 6){
+      $("#dice-2 img").attr('src', "./assets/images/dice-sides/side6.jpg");
+    }
+
+
     // $('#dice-1').attr('id', "dice" + x);
     // $('#dice-2').attr('id', "dice" + y);
     if (x == y) { //<----checking if there is a double
-
         dbl++; //<---increment double count
         alert("Doubles! Roll again. Double count: " + dbl);
         if(dbl%3==0){
           alert("Three doubles in a row, go to JAIL!");
           dbl = 0;
         }
-        //Now reroll the dice, but if you hit 3 doubles in a row, you get message go to jail.
-
     }
       });
 }
@@ -80,5 +108,3 @@ $("#info-btn").click(function (){
 $(".close").click (function(){
   $("#myModal").hide(300);
 });
-
-//
