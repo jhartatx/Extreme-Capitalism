74599b6c735ec24f8944e5bb807def027b4799d9
emmadae <emmadae@gmail.com>
2017-11-30 14:16:08 -0600

    update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,16 @@
-// module.exports = function(app) {
-//
+
+module.exports = function(app) {
+
 var path = require("path");
-//
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
-//
-// app.get("/checkplayers", function(req, res) {
-//   res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
-// };
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/checkplayers", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+};
 
 
 // Requiring our custom middleware for checking if a user is logged in
